c02264ce508474ce380cb619bd3ed926212fce75
vvalugonzalez <valu322@gmail.com>
2022-11-07 10:56:01 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './components/styles.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
-
+import ItemCount from './components/ItemCount';
 
 const App = () => {
 
+const stock = 10
     return(
     <div className='app' >
         <NavBar />
         <ItemListContainer greeting="Compre el servicio que necesita"/>
-        
+        <ItemCount stock = {stock}></ItemCount>
  
     </div>
     )
